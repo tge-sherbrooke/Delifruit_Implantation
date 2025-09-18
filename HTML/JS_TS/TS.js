c7a855c78ac8d90572bfd405a40d29bcf2235dc3
Ryan731-max <87987604+Ryan731-max@users.noreply.github.com>
2025-09-18 15:15:55 -0400

    UPDATES: images

--- HTML/JS_TS/TS.js
+++ HTML/JS_TS/TS.js
@@ -15,23 +15,39 @@ window.addEventListener('scroll', changerCouleurNav);
 
 //Deuxieme fonction Image
 
-function afficherImage()
-{
-    var imgZoom = document.querySelector('.modal');
-    var modalImage = document.querySelector('.modalImage');
+// function afficherImage()
+// {
+//     var imgZoom = document.querySelector('.modal');
+//     var modalImage = document.querySelector('.modalImage');
 
-    Array.from(document.querySelectorAll('.zoom')).forEach(
-      item => {item.addEventListener("click", event =>{
-        imgZoom.style.display="block";
-      modalImage.src=event.target.src
-    } );
+//     Array.from(document.querySelectorAll('.zoom')).forEach(
+//       item => {item.addEventListener("click", event =>{
+//         imgZoom.style.display="block";
+//       modalImage.src=event.target.src
+//     } );
+//   });
+
+//   document.querySelector(".close").addEventListener("click", () => {
+// imgZoom.style.display="none";
+//   });
+
+// }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  let sections = document.querySelectorAll("#multimediaContact, #GitHub_VS_Apache_Nginx, #formulaireContact", "#navbarInferieur");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("afficher");
+      }
+    });
+  }, { threshold: 0.1 });
+
+  sections.forEach(section => {
+    console.log("Section trouvée :", section.id);
+    observer.observe(section);
   });
-
-  document.querySelector(".close").addEventListener("click", () => {
-imgZoom.style.display="none";
-  });
-
-}
-
-
+});
 
